b843417b4d1a5ed31becdc023a7d8ad8ae2edfb0
AngeloC98 <acabrasnl@gmail.com>
2025-11-18 17:07:52 +0100

    feat: camera and viewport

--- src/vec3.h
+++ src/vec3.h
@@ -27,18 +27,48 @@ public:
         e[2] += v.e[2];
         return *this;
     }
+
+    double length() const
+    {
+        return std::sqrt(length_squared());
+    }
+
+    double length_squared() const
+    {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
 };
 
 using point3 = vec3;
 
 inline vec3 operator+(const vec3 &u, const vec3 &v)
 {
-    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[3] + v.e[3]);
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator+(double t, const vec3 &v)
+{
+    return vec3(t + v.e[0], t + v.e[1], t + v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
 inline vec3 operator*(double t, const vec3 &v)
 {
     return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator/(const vec3 &v, double t)
+{
+    return (1 / t) * v;
+}
+
+inline vec3 unit_vector(const vec3 &v)
+{
+    return v / v.length();
 }
 
 #endif
